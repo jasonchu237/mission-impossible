4ffc92e193255a8a71f82f8c307cdd0ca0cb4ca3
Derrick <jasonchu237@gmail.com>
2020-10-22 10:05:27 -0700

    Leetcode 0026 added

---
+++ Leetcode 0026 Remove Duplicates from Sorted Array [easy] [array]/Remove Duplicates from Sorted Array [two-pointers].js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let i = 0, j = 1;
+    while (j < nums.length) {
+        if (nums[j] === nums[i]) {
+            j++;
+        } else {
+            nums[++i] = nums[j];
+            j++;
+        }
+    }
+    
+    return i + 1;
+};
